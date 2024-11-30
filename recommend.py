b32d4d5aac95282f11e04e80a32749c890f91c5d
NIKHILJADON <e23cseu0040@bennett.edu.in>
2024-11-30 20:30:44 +0530

    Add files via upload

---
+++ recommend.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem.porter import PorterStemmer
+import ast
+
+# Load datasets
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv')
+
+# Merge datasets
+movies = movies.merge(credits, on='title')
+
+# Select important columns
+movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+movies.dropna(inplace=True)
+
+# Preprocessing functions
+def convert(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
+
+def fetch_director(obj):
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            return [i['name']]
+    return []
+
+def stem(text):
+    ps = PorterStemmer()
+    return " ".join([ps.stem(word) for word in text.split()])
+
+# Apply transformations
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(lambda x: [i['name'] for i in ast.literal_eval(x)[:3]])
+movies['crew'] = movies['crew'].apply(fetch_director)
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+# Combine all tags
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+movies['tags'] = movies['tags'].apply(lambda x: " ".join(x).lower())
+
+# Create new DataFrame
+new_df = movies[['movie_id', 'title', 'tags']]
+
+# Feature extraction
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(new_df['tags']).toarray()
+similarity = cosine_similarity(vectors)
+
+# Recommendation function
+def recommend(movie):
+    if movie not in new_df['title'].values:
+        return []
+    index = new_df[new_df['title'] == movie].index[0]
+    distances = similarity[index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:5]
+    return [new_df.iloc[i[0]].title for i in movie_list]
+
+
+#POSTERS -
+import requests
+
+# TMDB API Key
+TMDB_API_KEY = 'a98820b5938f3d1d8bdcfaf1984dcb09'
+
+def fetch_poster(movie_name):
+    # TMDB API endpoint
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={movie_name}"
+    response = requests.get(url)
+    data = response.json()
+    
+    if data['results']:
+        # Fetch the poster path of the first matching movie
+        poster_path = data['results'][0]['poster_path']
+        return f"https://image.tmdb.org/t/p/w500{poster_path}"  # Full poster URL
+    return None  # Return None if no poster found
